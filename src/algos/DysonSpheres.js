184dce7d63c8ce3f214fe21f6b129e1accb8bf63
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-05 00:05:44 +0100

    refactor: DysonSpheres

--- src/algos/DysonSpheres.js
+++ src/algos/DysonSpheres.js
@@ -1,64 +1,79 @@
-import { random, randomColor } from "../utils/random.js";
+import BaseAlgorithm from '../BaseAlgorithm.js';
 
-export default class DysonSpheres {
+export default class DysonSpheres extends BaseAlgorithm {
     constructor(ctx, w, h) {
-        this.ctx = ctx;
-        this.w = w;
-        this.h = h;
-        this.t = 0;
-        this.speed = random(2, 6);
-        this.interval = null;
+        super(ctx, w, h);
 
-        this.length = random(60, Math.max(this.h / 2, this.h - 60));
-        this.height = random(20, this.h / 2 - 40);
-        this.rot1 = random(1, 6);
+        this.initializeProperties();
+        this.setupDrawingStyles();
 
-        ctx.shadowBlur = 11;
-        ctx.shadowOffsetX = 1;
-        ctx.shadowOffsetY = 1;
-        ctx.shadowColor = ctx.strokeStyle = randomColor(5, 255, 0.33, 0.33);
-
-        this.draw = () => {
-            if (this.t % this.speed === 0) {
-                this.ctx.ellipse(
-                    this.w / 2,
-                    this.h / 2,
-                    this.length,
-                    this.height,
-                    this.rot1,
-                    0,
-                    0
-                );
-                this.ctx.stroke();
-            }
-            this.t++;
-            if (this.t % (this.speed * 170) === 0) {
-                this.ctx.beginPath();
-                let color = Math.random();
-                if (color < 0.2) {
-                    this.ctx.shadowBlur = 1;
-                    this.ctx.shadowOffsetX = 0;
-                    this.ctx.shadowOffsetY = 0;
-                    this.ctx.shadowColor = this.ctx.strokeStyle = 'black';
-                } else if (color < 0.4) {
-                    this.ctx.shadowBlur = 1;
-                    this.ctx.shadowOffsetX = 0;
-                    this.ctx.shadowOffsetY = 0;
-                    this.ctx.shadowColor = this.ctx.strokeStyle = 'white';
-                } else {
-                    this.ctx.shadowBlur = 11;
-                    this.ctx.shadowColor = this.ctx.strokeStyle = randomColor(
-                        5,
-                        255,
-                        0.33,
-                        0.33
-                    );
-                }
-                this.length = random(60, Math.max(this.h / 2, this.h - 60));
-                this.height = random(20, this.h / 2 - 40);
-            }
-            this.rot1 = random(0, 360);
-            this.interval = requestAnimationFrame(this.draw);
-        };
+        this.interval = requestAnimationFrame(this.draw);
     }
+
+    initializeProperties() {
+        this.length = this.random(60, Math.max(this.h / 2, this.h - 60));
+        this.height = this.random(20, this.h / 2 - 40);
+        this.rot1 = this.random(1, 6);
+    }
+
+    setupDrawingStyles() {
+        this.ctx.shadowBlur = 11;
+        this.ctx.shadowOffsetX = 1;
+        this.ctx.shadowOffsetY = 1;
+        this.ctx.shadowColor = this.ctx.strokeStyle = this.randomColor(
+            5,
+            255,
+            0.33,
+            0.33
+        );
+    }
+
+    draw = () => {
+        if (this.t % this.speed === 0) {
+            this.ctx.ellipse(
+                this.w / 2,
+                this.h / 2,
+                this.length,
+                this.height,
+                this.rot1,
+                0,
+                0
+            );
+            this.ctx.stroke();
+        }
+
+        if (this.t % (this.speed * 170) === 0) {
+            this.ctx.beginPath();
+
+            let color = Math.random();
+            if (color < 0.2) {
+                this.ctx.shadowBlur = 1;
+                this.ctx.shadowOffsetX = 0;
+                this.ctx.shadowOffsetY = 0;
+                this.ctx.shadowColor = this.ctx.strokeStyle = 'black';
+            } else if (color < 0.4) {
+                this.ctx.shadowBlur = 1;
+                this.ctx.shadowOffsetX = 0;
+                this.ctx.shadowOffsetY = 0;
+                this.ctx.shadowColor = this.ctx.strokeStyle = 'white';
+            } else {
+                this.ctx.shadowBlur = 11;
+                this.ctx.shadowColor = this.ctx.strokeStyle = this.randomColor(
+                    5,
+                    255,
+                    0.33,
+                    0.33
+                );
+            }
+
+            this.length = this.random(60, Math.max(this.h / 2, this.h - 60));
+            this.height = this.random(20, this.h / 2 - 40);
+        }
+
+        this.rot1 = this.random(0, 360);
+
+        this.t++;
+
+        requestAnimationFrame(this.draw);
+    };
 }
