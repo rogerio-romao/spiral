fe4529383e27eb2571aa01782a77c6b5f9ce3a6b
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-05 00:12:17 +0100

    refactor: Nebulas

--- src/algos/Nebulas.js
+++ src/algos/Nebulas.js
@@ -1,50 +1,68 @@
-import { random, randomColor } from "../utils/random.js";
+import BaseAlgorithm from '../BaseAlgorithm.js';
 
-export default class Nebulas {
+export default class Nebulas extends BaseAlgorithm {
     constructor(ctx, w, h) {
-        this.ctx = ctx;
-        this.w = w;
-        this.h = h;
-        this.t = 0;
-        this.speed = random(2, 6);
-        this.interval = null;
+        super(ctx, w, h);
 
-        this.length = random(50, Math.min(this.w, this.h) / 1.5);
-        this.gap = random(4, 100);
-        this.rotate = random(3, 160);
+        this.initializeProperties();
+        this.setupDrawingStyles();
 
+        this.interval = requestAnimationFrame(this.draw);
+    }
+
+    initializeProperties() {
+        this.length = this.random(50, Math.min(this.w, this.h) / 1.5);
+        this.gap = this.random(4, 100);
+        this.rotate = this.random(3, 160);
+    }
+
+    setupDrawingStyles() {
         this.ctx.shadowBlur = 15;
         this.ctx.shadowColor = 'rgba(255,255,255,0.7)';
         this.ctx.shadowOffsetX = 5;
         this.ctx.shadowOffsetY = 5;
-        this.ctx.fillStyle = this.ctx.strokeStyle = randomColor(5, 255, 0.02, 0.02);
-
-        this.draw = () => {
-            this.ctx.lineWidth = random(1, 200);
-            if (this.t % this.speed === 0) {
-                this.ctx.strokeRect(this.w / 2, this.h / 2, this.length, this.gap);
-                this.ctx.translate(this.w / 2, this.h / 2);
-                this.ctx.rotate(this.rotate);
-                this.ctx.translate(-this.w / 2, -this.h / 2);
-                this.length = random(10, Math.max(this.w, this.h));
-                this.gap += random(2, 10);
-                if (this.gap > 1000) this.gap = 1;
-                this.rotate = random(3, 160);
-            }
-            this.t++;
-            if (this.t % (this.speed * 10) === 0) {
-                this.ctx.fillRect(random(0, this.w), random(0, this.h), this.gap, this.gap);
-            }
-            if (this.t % (this.speed * 70) === 0) {
-                this.rotate = -this.rotate;
-                this.ctx.fillStyle = this.ctx.strokeStyle = randomColor(
-                    5,
-                    255,
-                    0.02,
-                    0.02
-                );
-            }
-            this.interval = requestAnimationFrame(this.draw);
-        };
+        this.ctx.fillStyle = this.ctx.strokeStyle = this.randomColor(
+            5,
+            255,
+            0.02,
+            0.02
+        );
     }
+
+    draw = () => {
+        if (this.t % this.speed === 0) {
+            this.ctx.lineWidth = this.random(1, 200);
+            this.ctx.strokeRect(this.w / 2, this.h / 2, this.length, this.gap);
+            this.ctx.translate(this.w / 2, this.h / 2);
+            this.ctx.rotate(this.rotate);
+            this.ctx.translate(-this.w / 2, -this.h / 2);
+            this.length = this.random(10, Math.max(this.w, this.h));
+            this.rotate = this.random(3, 160);
+            this.gap += this.random(2, 10);
+            if (this.gap > 1000) this.gap = 1;
+        }
+
+        if (this.t % (this.speed * 10) === 0) {
+            this.ctx.fillRect(
+                this.random(0, this.w),
+                this.random(0, this.h),
+                this.gap,
+                this.gap
+            );
+        }
+
+        if (this.t % (this.speed * 70) === 0) {
+            this.rotate = -this.rotate;
+            this.ctx.fillStyle = this.ctx.strokeStyle = this.randomColor(
+                5,
+                255,
+                0.02,
+                0.02
+            );
+        }
+
+        this.t++;
+
+        requestAnimationFrame(this.draw);
+    };
 }
