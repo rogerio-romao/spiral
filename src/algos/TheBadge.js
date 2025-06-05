6c730fcfafefc691bc4b4d186d3c94d9d14f32b9
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-05 11:37:55 +0100

    refactor: update TheBadge class

--- src/algos/TheBadge.js
+++ src/algos/TheBadge.js
@@ -1,20 +1,22 @@
-import { random } from "../utils/random.js";
+import BA from '../BaseAlgorithm.js';
 
-export default class TheBadge {
+export default class TheBadge extends BA {
     constructor(ctx, w, h) {
-        this.ctx = ctx;
-        this.w = w;
-        this.h = h;
-        this.speed = random(2, 6);
-        this.t = 0;
-        this.interval = null;
+        super(ctx, w, h);
 
-        this.length = random(50, Math.min(this.w, this.h) / 1.5);
-        this.rot1 = (random(0, 360) * Math.PI) / 180;
-        this.randCol = random(0, 255);
-        this.ctx.fillStyle = `rgb(${this.randCol + random(-28, 28)},${
-            this.randCol + random(-28, 28)
-        },${this.randCol + random(-28, 28)})`;
+        this.initializeProperties();
+        this.setupDrawingStyles();
+
+        this.interval = requestAnimationFrame(this.draw);
+    }
+
+    initializeProperties() {
+        this.length = BA.random(50, Math.min(this.w, this.h) / 1.5);
+        this.rot1 = (BA.random(0, 360) * Math.PI) / 180;
+        this.randCol = BA.random(0, 255);
+    }
+
+    setupDrawingStyles() {
         this.modes = [
             'difference',
             'soft-light',
@@ -25,42 +27,49 @@ export default class TheBadge {
             'source-atop',
         ];
 
+        this.ctx.fillStyle = `rgb(${this.randCol + BA.random(-28, 28)},${
+            this.randCol + BA.random(-28, 28)
+        },${this.randCol + BA.random(-28, 28)})`;
         this.ctx.strokeStyle = 'white';
         this.ctx.lineWidth = 3;
+    }
 
-        this.draw = () => {
-            if (this.t % this.speed === 0) {
-                this.ctx.fillRect(
-                    this.w / 2 - this.length * 1.5,
-                    this.h / 2 - this.length * 1.5,
-                    3 * this.length,
-                    3 * this.length
-                );
-                this.ctx.strokeRect(
-                    this.w / 2 - this.length * 1.5,
-                    this.h / 2 - this.length * 1.5,
-                    3 * this.length,
-                    3 * this.length
-                );
-                this.ctx.translate(this.w / 2, this.h / 2);
-                this.ctx.rotate(this.rot1);
-                this.ctx.translate(-this.w / 2, -this.h / 2);
-            }
-            this.t++;
-            if (this.t % (this.speed * 25) === 0) {
-                this.length = random(5, Math.min(this.w, this.h) / 3);
-                this.randCol = random(0, 255);
+    draw() {
+        if (this.t % this.speed === 0) {
+            this.ctx.fillRect(
+                this.w / 2 - this.length * 1.5,
+                this.h / 2 - this.length * 1.5,
+                3 * this.length,
+                3 * this.length
+            );
+            this.ctx.strokeRect(
+                this.w / 2 - this.length * 1.5,
+                this.h / 2 - this.length * 1.5,
+                3 * this.length,
+                3 * this.length
+            );
+            this.ctx.translate(this.w / 2, this.h / 2);
+            this.ctx.rotate(this.rot1);
+            this.ctx.translate(-this.w / 2, -this.h / 2);
+        }
 
-                this.ctx.fillStyle = `rgb(${this.randCol + random(-28, 28)},${
-                    this.randCol + random(-28, 28)
-                },${this.randCol + random(-28, 28)})`;
-            }
-            if (this.t % (this.speed * 50) === 0) {
-                this.rot1 = (random(0, 360) * Math.PI) / 180;
-                this.ctx.globalCompositeOperation =
-                    this.modes[random(0, this.modes.length - 1)];
-            }
-            this.interval = requestAnimationFrame(this.draw);
-        };
+        if (this.t % (this.speed * 25) === 0) {
+            this.length = BA.random(5, Math.min(this.w, this.h) / 3);
+            this.randCol = BA.random(0, 255);
+
+            this.ctx.fillStyle = `rgb(${this.randCol + BA.random(-28, 28)},${
+                this.randCol + BA.random(-28, 28)
+            },${this.randCol + BA.random(-28, 28)})`;
+        }
+
+        if (this.t % (this.speed * 50) === 0) {
+            this.rot1 = (BA.random(0, 360) * Math.PI) / 180;
+            this.ctx.globalCompositeOperation =
+                this.modes[BA.random(0, this.modes.length - 1)];
+        }
+
+        this.t++;
+
+        requestAnimationFrame(this.draw);
     }
 }
