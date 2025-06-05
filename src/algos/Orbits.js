2a208161dfac0c98ce310464ab6cecf4a3fa01c5
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-05 02:26:08 +0100

    refactor: Orbits class

--- src/algos/Orbits.js
+++ src/algos/Orbits.js
@@ -1,48 +1,56 @@
-import { random, randomColor } from "../utils/random.js";
+import BaseAlgorithm from '../BaseAlgorithm.js';
 
-export default class Orbits {
+export default class Orbits extends BaseAlgorithm {
     constructor(ctx, w, h) {
-        this.ctx = ctx;
-        this.w = w;
-        this.h = h;
-        this.speed = random(2, 6);
-        this.t = 0;
-        this.interval = null;
+        super(ctx, w, h);
 
-        this.radius = random(30, this.h);
-        this.radius2 = random(10, this.radius);
-        this.rot1 = random(1, 6);
-        this.startAngle = random(0, 100);
-        this.endAngle = random(101, 360);
+        this.initializeProperties();
+        this.setupDrawingStyles();
 
-        this.ctx.strokeStyle = randomColor(5, 255, 0.2, 0.2);
+        this.interval = requestAnimationFrame(this.draw);
+    }
 
-        this.draw = () => {
-            if (this.t % this.speed === 0) {
-                this.ctx.ellipse(
-                    this.w / 2,
-                    this.h / 2,
-                    this.radius,
-                    this.radius2,
-                    this.rot1,
-                    this.startAngle,
-                    this.endAngle
-                );
-            }
-            this.ctx.stroke();
-            this.ctx.translate(this.w / 2, this.h / 2);
-            this.ctx.rotate(this.rot1);
-            this.ctx.translate(-this.w / 2, -this.h / 2);
-            this.t++;
-            if (this.t % (this.speed * 150) === 0) {
-                this.ctx.beginPath();
-                this.ctx.strokeStyle = randomColor(5, 255, 0.2, 0.2);
-                this.radius = random(30, this.h);
-                this.radius2 = random(10, this.radius);
-                this.startAngle = random(0, 50);
-                this.rot1 = random(-3, 3);
-            }
-            interval = requestAnimationFrame(this.draw);
-        };
+    initializeProperties() {
+        this.radius = this.random(30, this.h);
+        this.radius2 = this.random(10, this.radius);
+        this.rot1 = this.random(1, 6);
+        this.startAngle = this.random(0, 100);
+        this.endAngle = this.random(101, 360);
+    }
+
+    setupDrawingStyles() {
+        this.ctx.strokeStyle = this.randomColor(5, 255, 0.2, 0.2);
+    }
+
+    draw() {
+        if (this.t % this.speed === 0) {
+            this.ctx.ellipse(
+                this.w / 2,
+                this.h / 2,
+                this.radius,
+                this.radius2,
+                this.rot1,
+                this.startAngle,
+                this.endAngle
+            );
+        }
+        this.ctx.stroke();
+
+        this.ctx.translate(this.w / 2, this.h / 2);
+        this.ctx.rotate(this.rot1);
+        this.ctx.translate(-this.w / 2, -this.h / 2);
+
+        if (this.t % (this.speed * 150) === 0) {
+            this.ctx.beginPath();
+            this.ctx.strokeStyle = this.randomColor(5, 255, 0.2, 0.2);
+            this.radius = this.random(30, this.h);
+            this.radius2 = this.random(10, this.radius);
+            this.startAngle = this.random(0, 50);
+            this.rot1 = this.random(-3, 3);
+        }
+
+        this.t++;
+
+        requestAnimationFrame(this.draw);
     }
 }
