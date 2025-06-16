a5c18bef9aae1270c19ae1091db251d54e7a2958
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-16 17:57:49 +0100

    feat: add custom fonts DM Mono and Oswald to the project

--- preload.js
+++ preload.js
@@ -1,9 +1,3 @@
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-    // Add any electron specific features you need exposed to the renderer
-});
-
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
         const element = document.getElementById(selector);
