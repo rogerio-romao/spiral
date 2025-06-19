eff0f4257a41052ba7727ff2f3b85fbd3a0543f5
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-19 08:11:54 +0100

    refactor:  DeepSea

--- src/algos/DeepSea.js
+++ src/algos/DeepSea.js
@@ -4,16 +4,21 @@ export default class DeepSea extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
+        this.setupConstantStyles();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
-    initializeProperties() {
+    initializeConstantProperties() {
         this.rotations = [
             4, 5, 6, 8, 9, 10, 12, 15, 18, 20, 24, 30, 36, 40, 45, 72, 90,
         ];
+    }
+
+    initializeProperties() {
         this.rot = this.rotations[AL.random(0, this.rotations.length)];
         this.startX = AL.random(0, this.w);
         this.startY = AL.random(0, this.h);
@@ -27,12 +32,15 @@ export default class DeepSea extends AL {
         this.factor2 = AL.random(36, 170);
     }
 
-    setupDrawingStyles() {
-        this.ctx.strokeStyle = AL.randomColor(50, 200, 0.35, 0.7);
-        this.ctx.shadowColor = AL.randomColor(75, 200, 0.3, 0.5);
+    setupConstantStyles() {
         this.ctx.lineWidth = 0.1;
         this.ctx.fillStyle = 'rgba(0, 0, 0, 0.045)';
         this.ctx.shadowBlur = 2;
+    }
+
+    setupDrawingStyles() {
+        this.ctx.strokeStyle = AL.randomColor(50, 200, 0.35, 0.7);
+        this.ctx.shadowColor = AL.randomColor(75, 200, 0.3, 0.5);
     }
 
     draw() {
@@ -54,29 +62,16 @@ export default class DeepSea extends AL {
             this.cp1x += Math.sin(this.t) * this.factor2;
             this.cp1y += Math.cos(this.t) * this.factor2;
         }
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate((this.rot * Math.PI) / 180);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
-
-        if (this.t % (this.speed * 270) === 0) {
-            this.startX = AL.random(0, this.w);
-            this.startY = AL.random(0, this.h);
-            this.cp1x = AL.random(0, this.w);
-            this.cp1y = AL.random(0, this.h);
-            this.cp2x = AL.random(0, this.w);
-            this.cp2y = AL.random(0, this.h);
-            this.endX = AL.random(0, this.w);
-            this.endY = AL.random(0, this.h);
-            this.factor = AL.random(180, 850);
-            this.factor2 = AL.random(36, 170);
-            this.rot = this.rotations[AL.random(0, this.rotations.length)];
-
-            this.ctx.beginPath();
-            this.ctx.strokeStyle = AL.randomColor(50, 200, 0.35, 0.7);
-            this.ctx.shadowColor = AL.randomColor(75, 255, 0.3, 0.5);
-        }
 
         this.t++;
+
+        this.rotateCanvasDegrees(this.rot);
+
+        if (this.t % (this.speed * 270) === 0) {
+            this.initializeProperties();
+            this.setupDrawingStyles();
+            this.ctx.beginPath();
+        }
 
         requestAnimationFrame(this.draw);
     }
