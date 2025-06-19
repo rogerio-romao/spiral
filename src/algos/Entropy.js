9d8f370bcd8c0267b7c39349e2b3ef2a9bd5f055
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-20 00:04:53 +0100

    refactor: update algorithm selection in chooseAlgos function and optimize drawing logic in Entropy class

--- src/algos/Entropy.js
+++ src/algos/Entropy.js
@@ -40,30 +40,23 @@ export default class Entropy extends AL {
             });
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate(this.rotate);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasRadians(this.rotate);
 
         if (this.t % (this.speed * 405) === 0) {
-            this.width = AL.random(50, this.w);
-            this.height = AL.random(50, this.h);
-            this.rotate = AL.random(1, 181);
-            this.ul = AL.random(10, Math.min(this.w, this.h));
-            this.ur = AL.random(10, Math.min(this.w, this.h));
-            this.ll = AL.random(10, Math.min(this.w, this.h));
-            this.lr = AL.random(10, Math.min(this.w, this.h));
+            this.initializeProperties();
 
-            let colorRoll = Math.random();
+            const colorRoll = Math.random();
             this.ctx.strokeStyle =
                 colorRoll < 0.1
                     ? 'black'
                     : colorRoll < 0.2
                     ? 'white'
                     : AL.randomColor(0, 255, 1);
+
             this.ctx.beginPath();
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
