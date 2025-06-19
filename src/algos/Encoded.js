551adefa5436cd9a8288168df69ef8eee6e2bd55
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-20 00:01:07 +0100

    refactor: update algorithm selection in chooseAlgos function and reorganize initialization in Encoded class

--- src/algos/Encoded.js
+++ src/algos/Encoded.js
@@ -4,34 +4,41 @@ export default class Encoded extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
+        this.setupConstantStyles();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
-    initializeProperties() {
+    initializeConstantProperties() {
         this.letters = ['S', 'P', 'I', 'R', 'A', 'L'];
-        this.letter = this.letters[AL.random(0, this.letters.length)];
-        this.size = AL.random(100, 400);
-        this.rot = (AL.random(1, 360) * Math.PI) / 180;
-        this.x = AL.random(0, this.w);
-        this.y = AL.random(0, this.h);
         this.angles = [10, 12, 15, 18, 20, 24, 36, 45, 72];
         this.angle = this.angles[AL.random(0, this.angles.length)];
     }
 
+    initializeProperties() {
+        this.letter = this.letters[AL.random(0, this.letters.length)];
+        this.size = AL.random(100, 400);
+        this.x = AL.random(0, this.w);
+        this.y = AL.random(0, this.h);
+    }
+
+    setupConstantStyles() {
+        this.ctx.shadowBlur = 10;
+        this.ctx.textAlign = 'center';
+        this.ctx.fillStyle = 'rgba(0,0,0,0.2)';
+    }
+
     setupDrawingStyles() {
+        this.ctx.font = `bold ${this.size}px serif`;
         this.ctx.shadowColor = this.ctx.strokeStyle = AL.randomColor(
             30,
             255,
             0.2,
             0.6
         );
-        this.ctx.fillStyle = 'rgba(0,0,0,0.2)';
-        this.ctx.font = `bold ${this.size}px serif`;
-        this.ctx.textAlign = 'center';
-        this.ctx.shadowBlur = 10;
     }
 
     draw() {
@@ -40,27 +47,14 @@ export default class Encoded extends AL {
             this.ctx.fillText(this.letter, this.x, this.y);
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate((this.angle * Math.PI) / 180);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasDegrees(this.angle);
 
         if (this.t % (this.speed * 72) === 0) {
-            this.letter = this.letters[AL.random(0, this.letters.length)];
-            this.size = AL.random(100, 400);
-            this.rot = (AL.random(1, 360) * Math.PI) / 180;
-            this.x = AL.random(0, this.w);
-            this.y = AL.random(0, this.h);
-
-            this.ctx.shadowColor = this.ctx.strokeStyle = AL.randomColor(
-                30,
-                255,
-                0.2,
-                0.6
-            );
-            this.ctx.font = `bold ${this.size}px serif`;
+            this.initializeProperties();
+            this.setupDrawingStyles();
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
