8aed758b4fcbb1ef92fe0f8c82d95b8e73b06bae
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-20 12:59:29 +0100

    refactor: update algorithm selection in chooseAlgos function and optimize drawing logic in Fluor class

--- src/algos/Fluor.js
+++ src/algos/Fluor.js
@@ -43,26 +43,16 @@ export default class Fluor extends AL {
             this.ctx.stroke();
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate((this.rotate * Math.PI) / 180);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasDegrees(this.rotate);
 
         if (this.t % (this.speed * 180) === 0) {
-            this.x1 = AL.random(0, this.w);
-            this.y1 = AL.random(0, this.h);
-            this.x2 = AL.random(0, this.w);
-            this.y2 = AL.random(0, this.h);
-            this.ox = AL.random(0, this.w);
-            this.oy = AL.random(0, this.h);
-            this.dx = AL.random(0, this.w);
-            this.dy = AL.random(0, this.h);
-            this.rotate = AL.random(1, 359);
+            this.initializeProperties();
 
             this.ctx.beginPath();
             this.ctx.strokeStyle = this.ctx.shadowColor = AL.randomColor();
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
