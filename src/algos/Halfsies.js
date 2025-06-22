f9f50d61d4cc0bc400ac933b38f5d199f8e3304d
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-22 16:56:54 +0100

    refactor: update algorithm selection in chooseAlgos function and reorganize initialization in Halfsies class

--- src/algos/Halfsies.js
+++ src/algos/Halfsies.js
@@ -4,9 +4,14 @@ export default class Halfsies extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
 
         this.interval = requestAnimationFrame(this.draw);
+    }
+
+    initializeConstantProperties() {
+        this.counter = false;
     }
 
     initializeProperties() {
@@ -14,7 +19,6 @@ export default class Halfsies extends AL {
         this.radius = AL.random(40, 400);
         this.x = AL.random(this.w / 2 - this.radius, this.w / 2 + this.radius);
         this.y = AL.random(this.h / 2 - this.radius, this.h / 2 + this.radius);
-        this.counter = false;
         this.width1 = AL.random(2, 11);
         this.width2 = AL.random(2, 11);
     }
@@ -22,7 +26,7 @@ export default class Halfsies extends AL {
     draw() {
         this.ctx.lineWidth = this.t % 2 ? this.width1 : this.width2;
         this.ctx.strokeStyle = this.t % 2 ? 'black' : 'white';
-        this.counter = this.t % 2 ? true : false;
+        this.counter = !this.counter;
         this.ctx.globalCompositeOperation =
             this.t % 2 ? 'source-over' : 'difference';
 
@@ -33,28 +37,15 @@ export default class Halfsies extends AL {
             this.ctx.closePath();
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate((this.rot * Math.PI) / 180);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasDegrees(this.rot);
 
         if (this.t % (this.speed * 120) === 0) {
-            this.rot = AL.random(3, 37);
-            this.width1 = AL.random(2, 11);
-            this.width2 = AL.random(2, 11);
-            this.radius = AL.random(40, 400);
-            this.x = AL.random(
-                this.w / 2 - this.radius,
-                this.w / 2 + this.radius
-            );
-            this.y = AL.random(
-                this.h / 2 - this.radius,
-                this.h / 2 + this.radius
-            );
+            this.initializeProperties();
 
             this.ctx.beginPath();
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
