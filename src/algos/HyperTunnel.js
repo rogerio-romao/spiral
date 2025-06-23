58df68cec512c8a0023bcf3efbc8598a9396d68e
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-23 17:30:33 +0100

    refactor: update algorithm selection in chooseAlgos function and streamline drawing logic in HyperTunnel class

--- src/algos/HyperTunnel.js
+++ src/algos/HyperTunnel.js
@@ -17,11 +17,7 @@ export default class HyperTunnel extends AL {
 
     setupDrawingStyles() {
         this.ctx.strokeStyle = AL.randomColor(5, 255, 0.25, 0.25);
-        if (this.side > Math.min(this.w, this.h) / 2) {
-            this.ctx.fillStyle = AL.randomColor(5, 255, 0.02, 0.02);
-        } else {
-            this.ctx.fillStyle = AL.randomColor(5, 255, 0.2, 0.2);
-        }
+        this.setFillStyle();
     }
 
     draw() {
@@ -46,36 +42,34 @@ export default class HyperTunnel extends AL {
             this.ctx.stroke();
             this.ctx.fill();
 
-            this.ctx.translate(this.w / 2, this.h / 2);
-            this.ctx.rotate((this.rotate * Math.PI) / 180);
-            this.ctx.translate(-this.w / 2, -this.h / 2);
+            this.rotateCanvasDegrees(this.rotate);
         }
+
+        this.t++;
 
         if (this.t % (this.speed * (360 / this.rotate)) === 0) {
             this.side = AL.random(25, Math.min(this.w, this.h));
 
+            this.setFillStyle();
             this.ctx.strokeStyle = AL.randomColor(5, 255, 0.25, 0.25);
-            if (this.side > Math.min(this.w, this.h) / 2) {
-                this.ctx.fillStyle = AL.randomColor(5, 255, 0.02, 0.02);
-            } else {
-                this.ctx.fillStyle = AL.randomColor(5, 255, 0.2, 0.2);
-            }
         }
 
         if (this.t % (this.speed * 75) === 0) {
             this.rotate = AL.random(95, 175);
             this.side = AL.random(25, Math.max(this.w, this.h));
 
+            this.setFillStyle();
             this.ctx.strokeStyle = AL.randomColor(5, 255, 0.25, 0.25);
-            if (this.side > Math.min(this.w, this.h) / 2) {
-                this.ctx.fillStyle = AL.randomColor(5, 255, 0.02, 0.02);
-            } else {
-                this.ctx.fillStyle = AL.randomColor(5, 255, 0.1, 0.1);
-            }
         }
 
-        this.t++;
-
         requestAnimationFrame(this.draw);
+    }
+
+    setFillStyle() {
+        if (this.side > Math.min(this.w, this.h) / 2) {
+            this.ctx.fillStyle = AL.randomColor(5, 255, 0.02, 0.02);
+        } else {
+            this.ctx.fillStyle = AL.randomColor(5, 255, 0.2, 0.2);
+        }
     }
 }
