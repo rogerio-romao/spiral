4b611b0fd601a5d0ec48bda65d8a80794a51d0e7
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-23 23:51:20 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance Lollipottery class initialization

--- src/algos/Lollipottery.js
+++ src/algos/Lollipottery.js
@@ -5,6 +5,7 @@ export default class Lollipottery extends AL {
         super(ctx, w, h);
 
         this.initializeProperties();
+        this.setupConstantStyles();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
@@ -15,12 +16,15 @@ export default class Lollipottery extends AL {
         this.alter = AL.random(-50, 50);
     }
 
+    setupConstantStyles() {
+        this.ctx.shadowBlur = 4;
+    }
+
     setupDrawingStyles() {
         this.ctx.lineWidth = AL.random(2, 14);
         this.ctx.globalCompositeOperation = 'overlay';
         this.ctx.strokeStyle = AL.randomColor();
         this.ctx.shadowColor = AL.randomColor();
-        this.ctx.shadowBlur = 4;
     }
 
     draw() {
@@ -29,8 +33,7 @@ export default class Lollipottery extends AL {
 
             this.radius += this.alter;
             if (this.radius > Math.max(this.w, this.h) || this.radius <= 40) {
-                this.radius = AL.random(50, Math.max(this.w, this.h) / 2);
-                this.alter = AL.random(-50, 50);
+                this.initializeProperties();
                 this.ctx.lineWidth = AL.random(2, 14);
             }
 
@@ -38,22 +41,17 @@ export default class Lollipottery extends AL {
             this.ctx.beginPath();
         }
 
-        if (this.t % (this.speed * 150) === 0) {
-            this.radius = AL.random(50, Math.max(this.w, this.h));
-            this.alter = AL.random(-50, 50);
+        this.t++;
 
-            this.ctx.lineWidth = AL.random(1, 12);
+        if (this.t % (this.speed * 150) === 0) {
+            this.initializeProperties();
+            this.setupDrawingStyles();
             this.ctx.beginPath();
-            this.ctx.strokeStyle = AL.randomColor();
-            this.ctx.shadowColor = AL.randomColor();
-            this.ctx.globalCompositeOperation = 'overlay';
         }
 
         if (this.t % (this.speed * 600) === 0) {
             this.ctx.globalCompositeOperation = 'source-over';
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
