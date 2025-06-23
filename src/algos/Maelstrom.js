ad16bc29146555339cb3705dfdbcdea22676ca7d
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-23 23:53:19 +0100

    refactor: update algorithm selection in chooseAlgos function and optimize drawing logic in Maelstrom class

--- src/algos/Maelstrom.js
+++ src/algos/Maelstrom.js
@@ -30,24 +30,18 @@ export default class Maelstrom extends AL {
             this.ctx.quadraticCurveTo(this.cp1, this.cp2, this.x1++, this.y1++);
             this.ctx.stroke();
 
-            this.ctx.translate(this.w / 2, this.h / 2);
-            this.ctx.rotate((this.angle * Math.PI) / 180);
-            this.ctx.translate(-this.w / 2, -this.h / 2);
+            this.rotateCanvasDegrees(this.angle);
         }
 
+        this.t++;
+
         if (this.t % (this.speed * 300) === 0) {
-            this.cp1 = AL.random(0, this.w);
-            this.cp2 = AL.random(0, this.h);
-            this.x1 = AL.random(0, this.w);
-            this.y1 = AL.random(0, this.h);
-            this.angle = AL.random(1, 200);
+            this.initializeProperties();
 
             this.ctx.fillRect(-this.w, -this.h, 3 * this.w, 3 * this.h);
             this.ctx.beginPath();
             this.ctx.strokeStyle = AL.randomColor(0, 255, 0.7, 1);
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
