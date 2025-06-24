15424a3ad50fb7f878d8ea40735f302f4df219cc
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-24 13:08:09 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in Majestic class

--- src/algos/Majestic.js
+++ src/algos/Majestic.js
@@ -4,22 +4,25 @@ export default class Majestic extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
-    initializeProperties() {
+    initializeConstantProperties() {
         this.rotations = [
             1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 17, 19, 20, 21, 22,
             23, 25, 26, 28, 29, 30, 31, 32, 33, 34, 35, 37, 38, 39, 40, 41, 42,
             43, 44, 45,
         ];
-        this.rotate = this.rotations[AL.random(0, this.rotations.length)];
+    }
 
-        this.x = AL.random(0, this.w);
-        this.y = AL.random(0, this.h);
+    initializeProperties() {
+        this.rotate = this.rotations[AL.random(0, this.rotations.length)];
+        this.x = AL.random(40, this.w - 40);
+        this.y = AL.random(40, this.h - 40);
         this.width = AL.random(30, this.w - 100);
         this.height = AL.random(30, this.h - 100);
         this.ul = AL.random(10, this.w);
@@ -51,27 +54,17 @@ export default class Majestic extends AL {
             );
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate((this.rotate * Math.PI) / 180);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasDegrees(this.rotate);
 
         if (this.t % (this.speed * 180) === 0) {
-            this.width = AL.random(30, this.w - 100);
-            this.height = AL.random(30, this.h - 100);
-            this.x = AL.random(0, this.w);
-            this.y = AL.random(0, this.h);
-            this.ul = AL.random(10, this.w);
-            this.ur = AL.random(10, this.h);
-            this.ll = AL.random(10, this.h);
-            this.lr = AL.random(10, this.w);
-            this.rotate = this.rotations[AL.random(0, this.rotations.length)];
+            this.initializeProperties();
 
             this.ctx.fillRect(-this.w, -this.h, 3 * this.w, 3 * this.h);
-            this.ctx.strokeStyle = AL.randomColor();
-            this.ctx.fillStyle = AL.randomColor(0, 255, 0.01, 0.05);
-        }
 
-        this.t++;
+            this.setupDrawingStyles();
+        }
 
         requestAnimationFrame(this.draw);
     }
