8c3d859da410bf494262ba80c3a53f238cc53f51
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-26 16:10:35 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in Orbits class

--- src/algos/Orbits.js
+++ src/algos/Orbits.js
@@ -4,18 +4,22 @@ export default class Orbits extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
+    initializeConstantProperties() {
+        this.rot = AL.random(1, 6);
+        this.endAngle = AL.random(101, 360);
+    }
+
     initializeProperties() {
         this.radius = AL.random(30, this.h);
         this.radius2 = AL.random(10, this.radius);
-        this.rot1 = AL.random(1, 6);
         this.startAngle = AL.random(0, 100);
-        this.endAngle = AL.random(101, 360);
     }
 
     setupDrawingStyles() {
@@ -29,27 +33,24 @@ export default class Orbits extends AL {
                 this.h / 2,
                 this.radius,
                 this.radius2,
-                this.rot1,
+                this.rot,
                 this.startAngle,
                 this.endAngle
             );
         }
         this.ctx.stroke();
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate(this.rot1);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasRadians(this.rot);
 
         if (this.t % (this.speed * 150) === 0) {
-            this.ctx.beginPath();
-            this.ctx.strokeStyle = AL.randomColor(5, 255, 0.2, 0.2);
-            this.radius = AL.random(30, this.h);
-            this.radius2 = AL.random(10, this.radius);
-            this.startAngle = AL.random(0, 50);
-            this.rot1 = AL.random(-3, 3);
-        }
+            this.initializeProperties();
+            this.rot = AL.random(-3, 3);
 
-        this.t++;
+            this.setupDrawingStyles();
+            this.ctx.beginPath();
+        }
 
         requestAnimationFrame(this.draw);
     }
