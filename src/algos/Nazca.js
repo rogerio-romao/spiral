0cc8321b431b38b046f4634dc3db932e7de5f9d5
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-26 10:45:57 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in Nazca class

--- src/algos/Nazca.js
+++ src/algos/Nazca.js
@@ -4,18 +4,22 @@ export default class Nazca extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
+    initializeConstantProperties() {
+        this.cycles = 0;
+        this.modes = ['soft-light', 'overlay', 'color'];
+    }
+
     initializeProperties() {
         this.r = 1;
         this.i = AL.random(13, 60);
         this.a = AL.random(1, 180);
-        this.cycles = 0;
-        this.modes = ['soft-light', 'overlay', 'color'];
     }
 
     setupDrawingStyles() {
@@ -38,28 +42,25 @@ export default class Nazca extends AL {
             this.r += this.i;
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate(-this.a);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasRadians(-this.a);
 
         if (this.r > Math.max(this.w, this.h)) {
             this.cycles++;
             if (this.cycles % 10 === 0) {
-                this.ctx.globalCompositeOperation =
-                    this.modes[AL.random(0, this.modes.length)];
+                this.ctx.globalCompositeOperation = AL.pickRandomElement(
+                    this.modes
+                );
             }
 
-            this.a = AL.random(1, 180);
-            this.r = 1;
-            this.i = AL.random(13, 60);
+            this.initializeProperties();
 
             this.ctx.lineWidth = AL.random(1, 7);
             this.ctx.fillStyle = AL.randomColor(0, 255, 0.15, 0.55);
             this.ctx.strokeStyle = AL.randomColor(0, 255, 0.75, 1);
             this.ctx.beginPath();
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
