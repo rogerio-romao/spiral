68526ce4bbedce91086a7245f1aa99847cd8ea79
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-26 16:18:59 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in Ourobouros class

--- src/algos/Ourobouros.js
+++ src/algos/Ourobouros.js
@@ -4,32 +4,37 @@ export default class Ourobouros extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
+        this.setupConstantStyles();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
-    initializeProperties() {
+    initializeConstantProperties() {
         this.letters = [
             2503, 2504, 2508, 2509, 2510, 2519, 2527, 2528, 2529, 2530, 2531,
             2536, 2537, 2539, 2541, 2544, 2545, 2563, 2566, 2569, 2584, 2591,
             2596,
         ];
-        this.letter = String.fromCharCode(
-            this.letters[AL.random(0, this.letters.length)]
-        );
+        this.letter = String.fromCharCode(AL.pickRandomElement(this.letters));
+    }
 
+    initializeProperties() {
         this.x = AL.random(100, this.w - 100);
         this.y = AL.random(100, this.h - 100);
         this.rotate = AL.random(4, 30);
     }
 
-    setupDrawingStyles() {
+    setupConstantStyles() {
         this.ctx.globalCompositeOperation = 'difference';
         this.ctx.lineWidth = 20;
-        this.ctx.font = `${AL.random(40, 300)}px sans-serif`;
         this.ctx.textAlign = 'center';
+    }
+
+    setupDrawingStyles() {
+        this.ctx.font = `${AL.random(40, 300)}px sans-serif`;
         this.ctx.strokeStyle = AL.randomColor(0, 255, 0.08, 0.4);
     }
 
@@ -38,26 +43,20 @@ export default class Ourobouros extends AL {
             this.ctx.strokeText(this.letter, this.x, this.y);
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate((this.rotate * Math.PI) / 180);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasDegrees(this.rotate);
 
         if (this.t % (this.speed * 80) === 0) {
-            this.x = AL.random(100, this.w - 100);
-            this.y = AL.random(100, this.h - 100);
-            this.rotate = AL.random(4, 30);
-
-            this.ctx.font = `${AL.random(40, 300)}px sans-serif`;
-            this.ctx.strokeStyle = AL.randomColor(0, 255, 0.08, 0.4);
+            this.initializeProperties();
+            this.setupDrawingStyles();
         }
 
         if (this.t % (this.speed * 1000) === 0) {
             this.letter = String.fromCharCode(
-                this.letters[AL.random(0, this.letters.length)]
+                AL.pickRandomElement(this.letters)
             );
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
