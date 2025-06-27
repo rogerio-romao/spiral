6e79080aba1935f1971d8c76672c54cc45509233
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-27 23:33:38 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in Polyhedra class

--- src/algos/Polyhedra.js
+++ src/algos/Polyhedra.js
@@ -4,20 +4,24 @@ export default class Polyhedra extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
-    initializeProperties() {
-        this.x = AL.random(0, this.w);
-        this.y = AL.random(0, this.h);
+    initializeConstantProperties() {
         this.rotations = [
             1, 2, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 17, 19, 20, 21, 23, 27,
             28, 29,
         ];
-        this.rotate = this.rotations[AL.random(0, this.rotations.length)];
+    }
+
+    initializeProperties() {
+        this.x = AL.random(0, this.w);
+        this.y = AL.random(0, this.h);
+        this.rotate = AL.pickRandomElement(this.rotations);
     }
 
     setupDrawingStyles() {
@@ -28,21 +32,17 @@ export default class Polyhedra extends AL {
         if (this.t % this.speed === 0) {
             this.ctx.lineTo(this.x, this.y);
             this.ctx.fill();
-            this.ctx.translate(this.w / 2, this.h / 2);
-            this.ctx.rotate(this.rotate);
-            this.ctx.translate(-this.w / 2, -this.h / 2);
-        }
 
-        if (this.t % (this.speed * 60) === 0) {
-            this.x = AL.random(0, this.w);
-            this.y = AL.random(0, this.h);
-            this.rotate = this.rotations[AL.random(0, this.rotations.length)];
-
-            this.ctx.beginPath();
-            this.ctx.fillStyle = AL.randomColor(0, 255, 0.01, 0.05);
+            this.rotateCanvasRadians(this.rotate);
         }
 
         this.t++;
+
+        if (this.t % (this.speed * 60) === 0) {
+            this.initializeProperties();
+            this.setupDrawingStyles();
+            this.ctx.beginPath();
+        }
 
         requestAnimationFrame(this.draw);
     }
