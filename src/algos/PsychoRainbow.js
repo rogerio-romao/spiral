2f557cbc413c0bb3f38b9dcff1e432ff49cc5012
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-28 20:48:44 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in PsychoRainbow class

--- src/algos/PsychoRainbow.js
+++ src/algos/PsychoRainbow.js
@@ -4,16 +4,19 @@ export default class PsychoRainbow extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
-    initializeProperties() {
+    initializeConstantProperties() {
         this.blends = ['hard-light', 'difference', 'color', 'luminosity'];
-        this.blend = this.blends[AL.random(0, this.blends.length)];
+        this.blend = AL.pickRandomElement(this.blends);
+    }
 
+    initializeProperties() {
         this.rows = AL.random(3, 10);
         this.rot = AL.random(1, 50);
         this.height = this.h / this.rows;
@@ -40,26 +43,18 @@ export default class PsychoRainbow extends AL {
             }
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate(this.rot);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasRadians(this.rot);
 
         if (this.t % (this.speed * 100) === 0) {
-            this.rows = AL.random(3, 10);
-            this.rot = AL.random(1, 50);
-            this.height = this.h / this.rows;
-            this.colors = [];
-            for (let i = 0; i <= this.rows; i++) {
-                this.colors.push(AL.randomColor(0, 255, 0.1, 0.5));
-            }
+            this.initializeProperties();
         }
 
         if (this.t % (this.speed * 700) === 0) {
-            this.blend = this.blends[AL.random(0, this.blends.length)];
-            this.ctx.globalCompositeOperation = this.blend;
+            this.blend = AL.pickRandomElement(this.blends);
+            this.setupDrawingStyles();
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
