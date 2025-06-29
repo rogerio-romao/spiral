13fb9ee86550e0f488cc8696ae396e575906b121
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-29 22:31:31 +0100

    refactor: update algorithm selection in chooseAlgos function and optimize drawing logic in Smooth class

--- src/algos/Smooth.js
+++ src/algos/Smooth.js
@@ -26,20 +26,14 @@ export default class Smooth extends AL {
             this.ctx.fillRect(this.x, this.y, this.size, this.size);
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate(this.rot);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasRadians(this.rot);
 
         if (this.t % (this.speed * 60) === 0) {
-            this.size = AL.random(50, 500);
-            this.x = AL.random(0, this.w);
-            this.y = AL.random(0, this.h);
-            this.rot = AL.random(1, 11);
-
-            this.ctx.fillStyle = AL.randomColor(0, 255, 0.03, 0.08);
+            this.initializeProperties();
+            this.setupDrawingStyles();
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
