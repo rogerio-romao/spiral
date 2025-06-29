a5786ee78edcc4dcc871fd2ba7a589d78fcfaecd
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-29 17:05:22 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in Sandala class

--- src/algos/Sandala.js
+++ src/algos/Sandala.js
@@ -4,42 +4,44 @@ export default class Sandala extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
-    initializeProperties() {
+    initializeConstantProperties() {
         this.letters = [
             3201, 3202, 3203, 3204, 3206, 3207, 3208, 3209, 3212, 3214, 3215,
             3218, 3219, 3221, 3222, 3223, 3226, 3227, 3228, 3231, 3232, 3234,
             3236, 3238, 3244, 3248, 3249, 3250, 3254, 3255, 3260, 3261, 3263,
             3270, 3294, 3298,
         ];
+    }
 
+    initializeProperties() {
         this.cols = AL.random(5, 12);
         this.rows = AL.random(5, 12);
         this.rot = AL.random(1, 60);
     }
 
+    setupConstantStyles() {
+        this.ctx.globalCompositeOperation = 'soft-light';
+    }
+
     setupDrawingStyles() {
         this.ctx.strokeStyle = AL.randomColor(0, 255, 1, 1);
-        this.ctx.globalCompositeOperation = 'soft-light';
     }
 
     draw() {
         if (this.t % this.speed === 0) {
             for (let row = 0; row <= this.rows; row++) {
-                this.ctx.translate(this.w / 2, this.h / 2);
-                this.ctx.rotate((this.rot * Math.PI) / 180);
-                this.ctx.translate(-this.w / 2, -this.h / 2);
+                this.rotateCanvasRadians(this.rot);
 
                 for (let col = 0; col <= this.cols; col++) {
                     this.ctx.strokeText(
-                        String.fromCharCode(
-                            this.letters[AL.random(0, this.letters.length)]
-                        ),
+                        String.fromCharCode(AL.pickRandomElement(this.letters)),
                         row * (this.w / this.cols),
                         col * (this.h / this.rows)
                     );
@@ -47,15 +49,12 @@ export default class Sandala extends AL {
             }
         }
 
-        if (this.t % (this.speed * 80) === 0) {
-            this.cols = AL.random(5, 12);
-            this.rows = AL.random(5, 12);
-            this.rot = AL.random(1, 60);
-
-            this.ctx.strokeStyle = AL.randomColor(0, 255, 1, 1);
-        }
-
         this.t++;
+
+        if (this.t % (this.speed * 80) === 0) {
+            this.initializeProperties();
+            this.setupDrawingStyles();
+        }
 
         requestAnimationFrame(this.draw);
     }
