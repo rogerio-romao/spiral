99717df7d8216177df7e2432005188a9df37ef30
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-30 09:27:31 +0100

    refactor: update algorithm selection in chooseAlgos function and optimize drawing logic in Solar class

--- renderer.js
+++ renderer.js
@@ -391,7 +391,7 @@ const LAST_ALGOS = [];
 function chooseAlgos() {
     let picks = ALGOS.filter((algo) => !LAST_ALGOS.includes(algo));
     // let choose = picks[random(0, picks.length)];
-    let choose = 'soapy-bubbles'; // for testing purposes
+    let choose = 'solar'; // for testing purposes
 
     LAST_ALGOS.push(choose);
     if (LAST_ALGOS.length > 58) LAST_ALGOS.shift();
--- src/algos/Solar.js
+++ src/algos/Solar.js
@@ -37,22 +37,16 @@ export default class Solar extends AL {
             this.ctx.stroke();
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate(this.rotate);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasRadians(this.rotate);
 
         if (this.t % (this.speed * 120) === 0) {
-            this.x1 = AL.random(0, this.w);
-            this.y1 = AL.random(0, this.h);
-            this.x2 = AL.random(0, this.w);
-            this.y2 = AL.random(0, this.h);
-            this.rotate = AL.random(1, 359);
+            this.initializeProperties();
 
             this.ctx.beginPath();
             this.ctx.strokeStyle = AL.randomColor();
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
