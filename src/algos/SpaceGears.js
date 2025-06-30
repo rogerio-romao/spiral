82f0a0400d40eeda85fbfa950902da253d79ef4c
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-06-30 09:32:35 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in SpaceGears class

--- src/algos/SpaceGears.js
+++ src/algos/SpaceGears.js
@@ -4,24 +4,28 @@ export default class SpaceGears extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
-    initializeProperties() {
+    initializeConstantProperties() {
         this.letters = [
             402, 406, 407, 409, 410, 412, 414, 415, 418, 420, 423, 424, 425,
             428, 429, 430, 433, 437, 438, 439, 440, 443, 444, 448, 449, 450,
             451, 458, 461, 474, 478, 480, 484, 488, 491, 494,
         ];
         this.letter = String.fromCharCode(428);
+    }
+
+    initializeProperties() {
         this.rotate = (AL.random(3, 357) * Math.PI) / 180;
-        this.ctx.font = AL.random(100, 700) + 'px serif';
     }
 
     setupDrawingStyles() {
+        this.ctx.font = AL.random(100, 700) + 'px serif';
         this.ctx.strokeStyle = AL.randomColor(0, 255, 0.6, 0.6);
     }
 
@@ -36,19 +40,18 @@ export default class SpaceGears extends AL {
             this.ctx.translate(-this.w / 2, -this.h / 2);
         }
 
+        this.t++;
+
         if (this.t % (this.speed * 120) === 0) {
-            this.ctx.strokeStyle = AL.randomColor(0, 255, 0.6, 0.6);
-            this.ctx.font = AL.random(100, 700) + 'px serif';
+            this.setupDrawingStyles();
         }
 
         if (this.t % (this.speed * 1260) === 0) {
-            this.rotate = (AL.random(3, 357) * Math.PI) / 180;
+            this.initializeProperties();
             this.letter = String.fromCharCode(
-                this.letters[AL.random(0, this.letters.length)]
+                AL.pickRandomElement(this.letters)
             );
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
