8c1d30e193e1c092cb41fbb87aa5e4d27e3c8929
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-07-01 20:48:47 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in SpiralText and SpiralLines classes

--- src/algos/SpiralLines.js
+++ src/algos/SpiralLines.js
@@ -14,7 +14,7 @@ export default class SpiralLines extends AL {
         this.radius = AL.random(10, this.h);
         this.length = AL.random(50, Math.min(this.w, this.h) / 1.5);
         this.gap = AL.random(4, 100);
-        this.rot1 = (AL.random(1, 359) * Math.PI) / 180;
+        this.rot = AL.random(1, 359);
         this.cycles = 1;
         this.bw = Math.random();
     }
@@ -58,17 +58,17 @@ export default class SpiralLines extends AL {
                 this.ctx.stroke();
             }
 
-            this.ctx.translate(this.w / 2, this.h / 2);
-            this.ctx.rotate(this.rot1);
-            this.ctx.translate(-this.w / 2, -this.h / 2);
+            this.rotateCanvasDegrees(this.rot);
 
             this.length += this.gap;
             if (this.length > Math.max(this.w, this.h)) {
                 this.cycles++;
                 this.length = this.gap;
+
                 this.ctx.beginPath();
                 this.ctx.strokeStyle = AL.randomColor(5, 255, 0.5, 0.5);
                 this.ctx.arc(this.w / 2, this.h / 2, this.radius, 0, 360);
+
                 this.bw = Math.random();
                 this.radius = AL.random(5, 65);
                 this.gap = AL.random(2, 30);
--- src/algos/SpiralText.js
+++ src/algos/SpiralText.js
@@ -5,6 +5,7 @@ export default class SpiralText extends AL {
         super(ctx, w, h);
 
         this.initializeProperties();
+        this.setupConstantStyles();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
@@ -13,25 +14,31 @@ export default class SpiralText extends AL {
     initializeProperties() {
         this.x = AL.random(0, this.w);
         this.y = AL.random(0, this.h);
-        this.rot1 = (AL.random(1, 20) * Math.PI) / 180;
+        this.rot = AL.random(1, 20);
         this.picker = AL.random(0, 11);
     }
 
-    setupDrawingStyles() {
+    setupConstantStyles() {
         this.ctx.globalCompositeOperation = 'color';
-        this.ctx.strokeStyle = AL.randomColor(0, 255, 1, 1);
         this.ctx.textAlign = 'center';
+    }
+
+    setupDrawingStyles() {
+        this.ctx.strokeStyle = AL.randomColor(0, 255, 1, 1);
         this.ctx.font = `bold ${AL.random(10, 400)}px sans-serif`;
     }
 
     draw() {
         if (this.t % this.speed === 0) {
             this.ctx.strokeText('SPIRAL', this.x, this.y);
-            this.ctx.translate(this.w / 2, this.h / 2);
-            this.ctx.rotate(this.rot1);
-            this.ctx.translate(-this.w / 2, -this.h / 2);
+
+            this.rotateCanvasDegrees(this.rot);
+
             this.x = AL.random(0, this.w);
         }
+
+        this.t++;
+
         if (this.t % (this.speed * 90) === 0) {
             this.y = AL.random(0, this.h);
             this.picker = AL.random(0, 11);
@@ -52,15 +59,12 @@ export default class SpiralText extends AL {
                 this.ctx.globalCompositeOperation = 'color';
             }
 
-            this.ctx.strokeStyle = AL.randomColor(0, 255, 1, 1);
-            this.ctx.font = `bold ${AL.random(10, 400)}px sans-serif`;
+            this.setupDrawingStyles();
         }
 
         if (this.t % (this.speed * 180) === 0) {
-            this.rot1 = (AL.random(1, 30) * Math.PI) / 180;
+            this.rot = AL.random(1, 30);
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
