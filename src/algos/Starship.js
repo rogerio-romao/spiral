d614fe256182097be3bc8b7944ea4b863d28b38f
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-07-02 21:23:45 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in Starship class

--- src/algos/Starship.js
+++ src/algos/Starship.js
@@ -4,30 +4,38 @@ export default class Starship extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
+        this.setupConstantStyles();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
-    initializeProperties() {
+    initializeConstantProperties() {
+        this.divisors = [2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 20, 24, 30, 36];
         this.first = 0;
         this.second = 1;
-        this.divisors = [2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 20, 24, 30, 36];
-        this.divisor = this.divisors[AL.random(0, this.divisors.length)];
         this.seq = [this.first, this.second];
     }
 
-    setupDrawingStyles() {
+    initializeProperties() {
+        this.divisor = AL.pickRandomElement(this.divisors);
+    }
+
+    setupConstantStyles() {
+        this.ctx.shadowBlur = 2;
+        this.ctx.lineWidth = 0.5;
         this.ctx.globalCompositeOperation = 'hard-light';
+    }
+
+    setupDrawingStyles() {
         this.ctx.shadowColor = this.ctx.strokeStyle = AL.randomColor(
             0,
             255,
             0.6,
             1
         );
-        this.ctx.shadowBlur = 2;
-        this.ctx.lineWidth = 0.5;
     }
 
     draw() {
@@ -41,33 +49,25 @@ export default class Starship extends AL {
                 this.second++;
                 this.seq = [this.first, this.second];
 
-                this.ctx.translate(this.w / 2, this.h / 2);
-                this.ctx.rotate(Math.PI / this.divisor);
-                this.ctx.translate(-this.w / 2, -this.h / 2);
+                this.rotateCanvasRadians(Math.PI / this.divisor);
             }
 
             this.ctx.arc(this.w / 2, this.h / 2, radius, 0, 2 * Math.PI);
             this.ctx.stroke();
         }
 
-        if (this.t % (this.speed * 240) === 0) {
-            this.ctx.beginPath();
-            this.ctx.shadowColor = this.ctx.strokeStyle = AL.randomColor(
-                0,
-                255,
-                0.6,
-                1
-            );
+        this.t++;
 
-            this.divisor = this.divisors[AL.random(0, this.divisors.length)];
+        if (this.t % (this.speed * 240) === 0) {
+            this.initializeProperties();
+            this.setupDrawingStyles();
+            this.ctx.beginPath();
         }
 
         if (this.t % (this.speed * 1200) === 0) {
             this.first = 0;
             this.second = 1;
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
