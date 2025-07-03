28366855d4c3831c1091f992e85ab0cc20d866fa
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-07-03 12:48:23 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance Swirls class initialization and drawing logic

--- src/algos/Swirls.js
+++ src/algos/Swirls.js
@@ -5,6 +5,7 @@ export default class Swirls extends AL {
         super(ctx, w, h);
 
         this.initializeProperties();
+        this.setupConstantStyles();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
@@ -20,9 +21,12 @@ export default class Swirls extends AL {
         this.rotate = AL.random(1, 10);
     }
 
+    setupConstantStyles() {
+        this.ctx.lineWidth = 0.4;
+    }
+
     setupDrawingStyles() {
         this.ctx.strokeStyle = AL.randomColor(0, 255, 0.5, 0.8);
-        this.ctx.lineWidth = 0.4;
     }
 
     draw() {
@@ -31,25 +35,16 @@ export default class Swirls extends AL {
             this.ctx.quadraticCurveTo(this.x2, this.y2, this.x1, this.y1);
             this.ctx.stroke();
 
-            this.ctx.translate(this.w / 2, this.h / 2);
-            this.ctx.rotate(this.rotate);
-            this.ctx.translate(-this.w / 2, -this.h / 2);
-        }
-
-        if (this.t % (this.speed * 240) === 0) {
-            this.x1 = AL.random(0, this.w);
-            this.y1 = AL.random(0, this.h);
-            this.x2 = AL.random(0, this.w);
-            this.y2 = AL.random(0, this.h);
-            this.x3 = AL.random(0, this.w);
-            this.y3 = AL.random(0, this.h);
-
-            this.ctx.beginPath();
-            this.ctx.strokeStyle = AL.randomColor(0, 255, 0.5, 0.8);
-            this.rotate = AL.random(1, 10);
+            this.rotateCanvasRadians(this.rotate);
         }
 
         this.t++;
+
+        if (this.t % (this.speed * 240) === 0) {
+            this.initializeProperties();
+            this.setupDrawingStyles();
+            this.ctx.beginPath();
+        }
 
         requestAnimationFrame(this.draw);
     }
