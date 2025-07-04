b4011875e559da2dd880081daa70d3b5ed2e99b0
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-07-04 16:30:23 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance initialization in Typobrush class

--- src/algos/Typobrush.js
+++ src/algos/Typobrush.js
@@ -4,23 +4,25 @@ export default class Typobrush extends AL {
     constructor(ctx, w, h) {
         super(ctx, w, h);
 
+        this.initializeConstantProperties();
         this.initializeProperties();
+        this.setupConstantStyles();
         this.setupDrawingStyles();
 
         this.interval = requestAnimationFrame(this.draw);
     }
 
-    initializeProperties() {
+    initializeConstantProperties() {
         this.letters = [
             2703, 2705, 2709, 2713, 2715, 2716, 2718, 2719, 2720, 2721, 2722,
             2725, 2726, 2731, 2732, 2735, 2738, 2739, 2741, 2742, 2743, 2745,
             2748, 2750, 2751, 2752, 2753, 2760, 2764, 2768, 2784, 2791, 2792,
             2795, 2796, 2797, 2798, 2799, 2800,
         ];
-        this.letter = String.fromCharCode(
-            this.letters[AL.random(0, this.letters.length)]
-        );
+        this.letter = String.fromCharCode(AL.pickRandomElement(this.letters));
+    }
 
+    initializeProperties() {
         this.x = AL.random(0, this.w);
         this.y = AL.random(0, this.h);
         this.size = 20;
@@ -28,10 +30,13 @@ export default class Typobrush extends AL {
         this.rot = AL.random(1, 400);
     }
 
-    setupDrawingStyles() {
-        this.ctx.strokeStyle = AL.randomColor(0, 255, 0.33, 0.33);
+    setupConstantStyles() {
         this.ctx.fillStyle = 'rgba(0, 0, 0, 0.75)';
         this.ctx.textAlign = 'center';
+    }
+
+    setupDrawingStyles() {
+        this.ctx.strokeStyle = AL.randomColor(0, 255, 0.33, 0.33);
         this.ctx.font = `${this.size}px serif`;
     }
 
@@ -42,30 +47,22 @@ export default class Typobrush extends AL {
             this.size += this.sizeInc;
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate(this.rot);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasRadians(this.rot);
 
         if (this.t % (this.speed * 150) === 0) {
-            this.size = 20;
-            this.x = AL.random(0, this.w);
-            this.y = AL.random(0, this.h);
-            this.rot = AL.random(1, 400);
-            this.sizeInc = AL.random(1, 6);
-
-            this.ctx.font = `${this.size}px serif`;
-            this.ctx.strokeStyle = AL.randomColor(0, 255, 0.33, 0.33);
+            this.initializeProperties();
+            this.setupDrawingStyles();
         }
 
         if (this.t % (this.speed * 1500) === 0) {
             this.letter = String.fromCharCode(
-                this.letters[AL.random(0, this.letters.length)]
+                AL.pickRandomElement(this.letters)
             );
 
             this.ctx.fillRect(-this.w, -this.h, 3 * this.w, 3 * this.h);
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
