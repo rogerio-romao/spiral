42732a0f5b55ebba3371a3f31a37c77235aa07db
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-07-04 19:24:12 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance drawing logic in Universe class

--- src/algos/Universe.js
+++ src/algos/Universe.js
@@ -111,41 +111,17 @@ export default class Universe extends AL {
                 );
             }
 
-            this.ctx.translate(this.w / 2, this.h / 2);
-            this.ctx.rotate(this.rotate);
-            this.ctx.translate(-this.w / 2, -this.h / 2);
+            this.rotateCanvasRadians(this.rotate);
         }
 
-        if (this.t % (this.speed * 250) === 0) {
-            this.rounded1 = AL.random(15, 50);
-            this.rounded2 = AL.random(15, 50);
-            this.rounded3 = AL.random(15, 50);
-            this.rounded4 = AL.random(15, 50);
-            this.x1 = AL.random(0, this.w / 2);
-            this.y1 = AL.random(0, this.h / 2);
-            this.x2 = AL.random(this.w / 2, this.w);
-            this.y2 = AL.random(0, this.h / 2);
-            this.x3 = AL.random(this.w / 2, this.w);
-            this.y3 = AL.random(this.h / 2, this.h);
-            this.x4 = AL.random(0, this.w / 2);
-            this.y4 = AL.random(this.h / 2, this.h);
-            this.side1 = AL.random(60, this.w / 2);
-            this.side2 = AL.random(60, this.h / 2);
-            this.side3 = AL.random(60, this.w / 2);
-            this.side4 = AL.random(60, this.h / 2);
-            this.side5 = AL.random(60, this.w / 2);
-            this.side6 = AL.random(60, this.h / 2);
-            this.side7 = AL.random(60, this.w / 2);
-            this.side8 = AL.random(60, this.h / 2);
-            this.rotate = AL.random(1, 11);
-
-            this.ctx.strokeStyle = AL.randomColor(0, 255, 0.5, 1);
-            this.ctx.fillStyle = AL.randomColor(0, 255, 0.01, 0.05);
-        }
+        this.t++;
 
         this.stagger++;
 
-        this.t++;
+        if (this.t % (this.speed * 250) === 0) {
+            this.initializeProperties();
+            this.setupDrawingStyles();
+        }
 
         requestAnimationFrame(this.draw);
     }
