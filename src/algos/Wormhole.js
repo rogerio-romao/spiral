965109a2fb7213877a14a99e819089c3163d24a0
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2025-07-05 10:02:48 +0100

    refactor: update algorithm selection in chooseAlgos function and enhance drawing logic in Wormhole class

--- src/algos/Wormhole.js
+++ src/algos/Wormhole.js
@@ -45,9 +45,9 @@ export default class Wormhole extends AL {
             );
         }
 
-        this.ctx.translate(this.w / 2, this.h / 2);
-        this.ctx.rotate(this.rotate);
-        this.ctx.translate(-this.w / 2, -this.h / 2);
+        this.t++;
+
+        this.rotateCanvasRadians(this.rotate);
 
         this.width++;
         this.height++;
@@ -55,21 +55,9 @@ export default class Wormhole extends AL {
         this.ur++;
 
         if (this.t % (this.speed * 360) === 0) {
-            this.width = AL.random(30, this.w);
-            this.height = AL.random(30, this.h);
-            this.x = AL.random(50, this.w - 50);
-            this.y = AL.random(50, this.h - 50);
-            this.ul = AL.random(10, 50);
-            this.ur = AL.random(10, 50);
-            this.ll = AL.random(10, 50);
-            this.lr = AL.random(10, 50);
-            this.rotate = AL.random(1, 44);
-
-            this.ctx.strokeStyle = AL.randomColor(0, 255, 0.2, 0.5);
-            this.ctx.fillStyle = AL.randomColor(0, 255, 0.01, 0.01);
+            this.initializeProperties();
+            this.setupDrawingStyles();
         }
-
-        this.t++;
 
         requestAnimationFrame(this.draw);
     }
