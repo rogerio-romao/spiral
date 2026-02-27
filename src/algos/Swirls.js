a4040abc74687171dcb8311f46416b9737cecd48
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2026-02-27 22:37:22 +0000

    fix: refactor property initialization and streamline drawing logic in Swirls for improved clarity

--- src/algos/Swirls.js
+++ src/algos/Swirls.js
@@ -8,7 +8,6 @@ export default class Swirls extends AL {
 
         this.initializeProperties();
         this.setupConstantStyles();
-        this.setupDrawingStyles();
 
         this.requestFrame();
     }
@@ -21,14 +20,16 @@ export default class Swirls extends AL {
         this.x3 = AL.random(0, this.w);
         this.y3 = AL.random(0, this.h);
         this.rotate = AL.random(1, 10);
+        this.numColors = 7;
+        this.mode = 'hue';
+        this.colors = AL.generateHSLAPalette(this.numColors, this.mode);
+        this.colorIndex = 0;
     }
 
     setupConstantStyles() {
-        this.ctx.lineWidth = 0.4;
-    }
-
-    setupDrawingStyles() {
-        this.ctx.strokeStyle = AL.randomColor(0, 255, 0.5, 0.8);
+        this.ctx.lineWidth = 2;
+        this.ctx.shadowColor = 'rgba(5, 5, 5, 0.7)';
+        this.ctx.shadowBlur = 3;
     }
 
     draw() {
@@ -37,15 +38,22 @@ export default class Swirls extends AL {
             this.ctx.quadraticCurveTo(this.x2, this.y2, this.x1, this.y1);
             this.ctx.stroke();
 
+            this.ctx.strokeStyle = this.colors[this.colorIndex];
+            this.colorIndex = (this.colorIndex + 1) % this.colors.length;
+
             this.rotateCanvasRadians(this.rotate);
         }
 
         this.t++;
 
         if (this.t % (this.speed * 240) === 0) {
+            this.ctx.closePath();
             this.initializeProperties();
-            this.setupDrawingStyles();
             this.ctx.beginPath();
+        }
+
+        if (this.t % (this.speed * 600) === 0) {
+            this.colors = AL.generateHSLAPalette(this.numColors, this.mode);
         }
 
         this.requestFrame();
