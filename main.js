2d27a5f1f2b5dbb90f99f2b12f8bd646dda4f3cf
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2026-03-01 01:04:01 +0000

    fix: set application menu to null and adjust window creation parameters

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu, screen } from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-function createWindow() {
+Menu.setApplicationMenu(null);
+
+function createWindow(width, height) {
     const mainWindow = new BrowserWindow({
-        width: 1600,
-        height: 1000,
+        width,
+        height,
+        useContentSize: true,
         webPreferences: {
             preload: path.join(
                 path.dirname(fileURLToPath(import.meta.url)),
@@ -16,32 +19,25 @@ function createWindow() {
         },
     });
 
-    // and load the index.html of the app.
     mainWindow.loadFile('index.html');
-
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createWindow();
+    // get screen size
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    createWindow(width, height);
 
     app.on('activate', function () {
         // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+        // dock icon is clicked and there are no other windows open. In this case, we check if there are no windows and create a new one, since we only want one window open at a time.
+        if (BrowserWindow.getAllWindows().length === 0)
+            createWindow(width, height);
     });
 });
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') app.quit();
+    app.quit();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
