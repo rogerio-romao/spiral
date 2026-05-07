18faa571ed87ab73172e581aba5568054c008983
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2026-05-07 11:13:56 +0100

    refactor: reorganize Division class
    chore: use internal ts version in vscode

--- src/algos/Division.js
+++ src/algos/Division.js
@@ -6,6 +6,7 @@ export default class Division extends AL {
 
         this.name = 'Division';
 
+        this.initializeConstantProperties();
         this.initializeProperties();
         this.setupConstantStyles();
         this.setupDrawingStyles();
@@ -13,9 +14,12 @@ export default class Division extends AL {
         this.requestFrame();
     }
 
+    initializeConstantProperties() {
+        this.circles = [6, 8, 10, 12, 16, 20, 24, 32, 40, 48];
+    }
+
     initializeProperties() {
         this.radius = AL.random(25, Math.min(AL.w, AL.h) / 2);
-        this.circles = [6, 8, 10, 12, 16, 20, 24, 32, 40, 48];
         this.size = AL.pickRandomElement(this.circles);
         this.angle = 0;
     }
@@ -27,14 +31,13 @@ export default class Division extends AL {
 
     setupDrawingStyles() {
         AL.ctx.strokeStyle = AL.randomColor();
-        AL.ctx.shadowColor = 'white';
         AL.ctx.fillStyle = AL.randomColor();
     }
 
     draw() {
         if (this.t % this.speed === 0) {
-            for (let i = 0; i < this.circles.length; i++) {
-                this.angle = (i * Math.PI * 2) / this.circles.length;
+            for (let i = 0; i < 10; i++) {
+                this.angle = (i * Math.PI * 2) / 10;
                 const x = AL.w / 2 + Math.cos(this.angle) * this.radius;
                 const y = AL.h / 2 + Math.sin(this.angle) * this.radius;
 
@@ -51,7 +54,7 @@ export default class Division extends AL {
             this.initializeProperties();
         }
 
-        if (this.t % (this.speed * 400) === 0) {
+        if (this.t % (this.speed * 320) === 0) {
             this.setupDrawingStyles();
         }
 
